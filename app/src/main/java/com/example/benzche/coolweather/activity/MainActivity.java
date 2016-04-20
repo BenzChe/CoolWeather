77fa5628d6b196e0253d3f3f3f389b655c79f6f4
Benz <amo17@126.com>
2016-04-20 11:17:32 +0800

    Second Commit

--- app/src/main/java/com/example/benzche/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/benzche/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.benzche.coolweather;
+package com.example.benzche.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.benzche.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
